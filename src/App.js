8edb3df4160fa37b2d80920c90243b64b0c61bfc
Walther Carrasco <walthercarrasco16@gmail.com>
2023-11-26 15:39:11 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './cosito.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          #TePisanKevin
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vm.tiktok.com/ZM6J5GeoL/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Apritame gay
         </a>
       </header>
     </div>
